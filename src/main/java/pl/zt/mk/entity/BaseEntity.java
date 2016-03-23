5d92c19071901670a8a295a90d307ee97456228a
Michal Kaczmarek <kapj-25@sr.dmcs.pl>
2016-03-23 20:08:53 +0100

    Usunięto setery i gettery oraz dodano adnotacje setter i getter w BaseEntity

--- src/main/java/pl/zt/mk/entity/BaseEntity.java
+++ src/main/java/pl/zt/mk/entity/BaseEntity.java
@@ -1,22 +1,19 @@
 package pl.zt.mk.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
 
 @Entity
+@Setter
+@Getter
 public abstract class BaseEntity {
 
 	@Id
 	private Long id;
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
 	
 }
