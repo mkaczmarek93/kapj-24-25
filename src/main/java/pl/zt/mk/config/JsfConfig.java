4c7ee3bd8829aa513907b36d3da398ebad941e1e
Zbyszek Tuszyński <kapj-24@sr.dmcs.pl>
2016-04-03 11:50:33 +0200

    security bean + renreded menu

--- src/main/java/pl/zt/mk/config/JsfConfig.java
+++ src/main/java/pl/zt/mk/config/JsfConfig.java
@@ -1,18 +1,16 @@
 package pl.zt.mk.config;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import javax.faces.webapp.FacesServlet;
-import javax.servlet.ServletContext;
-
 import org.springframework.beans.factory.config.CustomScopeConfigurer;
 import org.springframework.boot.context.embedded.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.context.ServletContextAware;
-
 import pl.zt.mk.jsf.ViewScope;
+
+import javax.faces.webapp.FacesServlet;
+import javax.servlet.ServletContext;
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 public class JsfConfig implements ServletContextAware {
@@ -31,7 +29,8 @@ public class JsfConfig implements ServletContextAware {
     	servletContext.setInitParameter("com.sun.faces.forceLoadConfiguration", Boolean.TRUE.toString());
         servletContext.setInitParameter("javax.faces.PROJECT_STAGE", "Development");
         servletContext.setInitParameter("primefaces.FONT_AWESOME", Boolean.TRUE.toString());
-    }	
+		servletContext.setInitParameter("javax.faces.FACELETS_LIBRARIES", "/WEB-INF/springsecurity.taglib.xml");
+	}
 
     @Bean
 	public static CustomScopeConfigurer customScopeConfigurer() {
